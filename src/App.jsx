d4d6d0efb070ccf25e16b7d85ff468e1619adc63
Guendouz mehdi <mahdign12@gmail.com>
2023-12-06 10:12:03 +0000

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import Hero from "./components/hero-section/Hero";
 import EventLocation from "./components/event-location/EventLocation";
 import EventAgenda from "./components/event-agenda/EventAgenda";
@@ -18,6 +17,14 @@ import AboutBytecraft from "./components/aboutBytecraft/AboutBytecraft";
 function App() {
   return (
     <>
+      <div class="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
+        <h1 class="text-5xl text-white font-bold mb-8 animate-pulse">
+          Coming Soon
+        </h1>
+        <p class="text-white text-lg mb-8">
+          We're working hard to bring you something amazing. Stay tuned!
+        </p>
+      </div>
       {/* <Slide> */}
       <Hero />
       {/* </Slide> */}
