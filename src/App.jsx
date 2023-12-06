6006108e5ba1f9afe605e79c5077a8d406d8f782
Guendouz mehdi <mahdign12@gmail.com>
2023-12-06 10:00:20 +0000

    remove react-full-page just for the deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { FullPage, Slide } from "react-full-page";
 import "./App.css";
 import Hero from "./components/hero-section/Hero";
 import EventLocation from "./components/event-location/EventLocation";
@@ -9,44 +8,44 @@ import Sponsors from "./components/sponsors/Sponsors";
 import MentorsJuries from "./components/mentors & juries/MentorsJuries";
 import AboutBytecraft from "./components/aboutBytecraft/AboutBytecraft";
 
-const fullPageOptions = {
-  scrollSensitivity: 3,
-  touchSensitivity: 1,
-  scrollSpeed: 400, // Adjust the scroll speed as needed
-  resetSlides: true,
-};
+// const fullPageOptions = {
+//   scrollSensitivity: 3,
+//   touchSensitivity: 1,
+//   scrollSpeed: 400, // Adjust the scroll speed as needed
+//   resetSlides: true,
+// };
 
 function App() {
   return (
-    <FullPage {...fullPageOptions}>
-      <Slide>
-        <Hero />
-      </Slide>
+    <>
+      {/* <Slide> */}
+      <Hero />
+      {/* </Slide> */}
 
-      <Slide>
-        <EventLocation />
-      </Slide>
+      {/* <Slide> */}
+      <EventLocation />
+      {/* </Slide> */}
 
-      <Slide>
-        <EventDate />
-      </Slide>
+      {/* <Slide> */}
+      <EventDate />
+      {/* </Slide> */}
 
-      <Slide>
-        <Sponsors />
-      </Slide>
+      {/* <Slide> */}
+      <Sponsors />
+      {/* </Slide> */}
 
-      <Slide>
-        <MentorsJuries />
-      </Slide>
+      {/* <Slide> */}
+      <MentorsJuries />
+      {/* </Slide> */}
 
-      <Slide>
-        <EventAgenda />
-      </Slide>
+      {/* <Slide> */}
+      <EventAgenda />
+      {/* </Slide> */}
 
-      <Slide>
-        <AboutBytecraft />
-      </Slide>
-    </FullPage>
+      {/* <Slide> */}
+      <AboutBytecraft />
+      {/* </Slide> */}
+    </>
   );
 }
 
